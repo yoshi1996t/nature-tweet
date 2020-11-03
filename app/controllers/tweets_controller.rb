45d88bfd9685b9b71ab0b73cad1e8b75c4372550
yoshi1996t <am.k.fs.sea0702@gmail.com>
2020-11-03 10:36:48 +0900

    Active Storage install

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,4 +2,10 @@ class TweetsController < ApplicationController
   def index
     @users = User.all
   end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
